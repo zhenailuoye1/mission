33587b138f5104a5811d64a3ed4c0122b1eb04bb
zhenailuoye1 <122676065@qq.com>
2016-06-28 13:37:49 +0800

    db

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sqlite3
 from flask import *
 app = Flask(__name__)
+
+def connect_db():
+    return sqlite3.connect('/path/to/database.db')
 
 @app.route('/')
 def hello_world():
@@ -8,8 +12,16 @@ def hello_world():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-       print(request.form['user_name'])
-       return "登陆成功"
+        u = request.form['user_name']
+        p = request.form['password']
+        n = request.form['email']
+        with connect_db() as con
+            cur = con.cursor()
+            cur.execute('SELECT password FROM account WHERE user_name = %s',(u,))
+            if u == cur.fetchone():
+                return render_template('login.html', state="登陆成功")
+            else:
+                return render_template('login.html', state="未登陆")
     else:
        return render_template('login.html', state="未登陆")
 
