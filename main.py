db1063e668c2e02c91d78f37cd4c493560b23d11
zhenailuoye1 <122676065@qq.com>
2016-06-29 13:36:45 +0800

    sqlite

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from flask import *
 app = Flask(__name__)
 
 def connect_db():
-    return sqlite3.connect('/path/to/database.db')
+    return sqlite3.connect('database.db')
+
+def get_db():
+    db = getattr(g, '_db', None)
+    if db is None:
+        db = g._db = connect_db()
+    return db
 
 @app.route('/')
 def hello_world():
@@ -12,18 +18,22 @@ def hello_world():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        u = request.form['user_name']
-        p = request.form['password']
-        n = request.form['email']
-        with connect_db() as con
-            cur = con.cursor()
-            cur.execute('SELECT password FROM account WHERE user_name = %s',(u,))
-            if u == cur.fetchone():
-                return render_template('login.html', state="登陆成功")
-            else:
-                return render_template('login.html', state="未登陆")
+        user_name = request.form['user_name']
+        password = request.form['password']
+        print(request.form['user_name'])
+        db =  get_db()
+        cursor = db.cursor()
+        cursor.execute('SELECT password FROM account WHERE name = ?',(user_name,))
+        pw = cursor.fetchone()
+        if password == pw:
+            return render_template('login.html', state="登陆成功")
+        return render_template('login.html', state="用户名或者密码错误")
     else:
         return render_template('login.html', state="未登陆")
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
