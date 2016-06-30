a74aaa0dd570819f79e07c80ce7c2e9f13d0cb69
zhenailuoye1 <122676065@qq.com>
2016-06-30 18:10:12 +0800

    login2.0

--- static/js/login.js
+++ static/js/login.js
@@ -4,6 +4,14 @@ $(function () {
     $('#btn-login').click(function () {
         var name = $('input[name="user_name"]').val();
         var pw = $('input[name="password"]').val();
+        if (!name) {
+            alert('用户名不能为空!');
+            return false;
+        }
+        if (!pw) {
+            alert('密码不能为空!')
+            return false;
+        }
         $.ajax({
             url: '/login',
             type: 'POST',
