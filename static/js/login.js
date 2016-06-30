6ea0b288b4022bc5be2b10c3f0d87d9c8bef53cc
zhenailuoye1 <122676065@qq.com>
2016-06-30 18:01:18 +0800

    login

--- static/js/login.js
+++ static/js/login.js
@@ -2,15 +2,22 @@ alert('请输入用户名和密码')
 
 $(function () {
     $('#btn-login').click(function () {
+        var name = $('input[name="user_name"]').val();
+        var pw = $('input[name="password"]').val();
         $.ajax({
             url: '/login',
             type: 'POST',
             data: {
-                user_name: $('input[name="user_name"]').val(),
-                password: $('input[name="password"]').val(),
+                user_name: name,
+                password: pw,
             },
-            success: function (data) {
-                alert(data);
+            success: function (data,Status) {
+                if (data == 'ok') {
+                    window.location.href = "/";
+                }
+                else {
+                alert("用户名或密码错误");
+                }
             }
         })
     })
