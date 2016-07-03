6d2ebf6c8ad8394170c820c05608eab358ad4d47
Soryu Asuka Langley <Soryu Asuka Langley>
2016-07-03 15:09:57 +0800

    bt2.3

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,5 @@
 $(function () {
+    //登录账号
     $('#btn-login').click(function () {
         var name = $('input[name="user_name"]').val();
         var pw = $('input[name="password"]').val();
@@ -19,11 +20,47 @@ $(function () {
             },
             success: function (data, Status) {
                 if (data == 'ok') {
-                    alert('登陆成功！跳转到首页中...')
+                    alert('登录成功！跳转到首页中...')
                     window.location.href = "/";
                 }
                 else {
                     alert("用户名或密码错误");
+                }
+            }
+        })
+    })
+    //注册账号
+    $('#btn-register').click(function () {
+        var em = $('input[name="re_email"]').val();
+        var un = $('input[name="re_user_name"]').val();
+        var pw = $('input[name="re_password"]').val();
+        var cf = $('input[name="re_confirm"]').val();
+        if (name.length < 6) {
+            alert('用户名至少6位');
+            return false;
+        } else if (pw.length < 6) {
+            alert('密码至少6位')
+            return false;
+        } else if (pw != cf) {
+            alert('两次密码不一致')
+        }
+
+        $.ajax({
+            url: '/login',
+            type: 'POST',
+            data: {
+                user_name: un,
+                password: pw,
+            },
+            success: function (data, Status) {
+                if (data == 'ok') {
+                    alert('注册成功！')
+                    window.location.href = "/login";
+                } else if (data == 'email exist') {
+                    alert('邮箱已注册');
+                }
+                else if (data == 'user_name exist') {
+                    alert('用户名已注册');
                 }
             }
         })
